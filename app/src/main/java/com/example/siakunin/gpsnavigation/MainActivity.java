582f4c6746911a79e3180ad2df3390ceb99bb286
siakunin <79031587670@mail.ru>
2018-01-12 17:23:49 +0300

    Change version and check permission on Runtime

--- app/src/main/java/com/example/siakunin/gpsnavigation/MainActivity.java
+++ app/src/main/java/com/example/siakunin/gpsnavigation/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.siakunin.gpsnavigation;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +17,13 @@ import android.widget.TextView;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] LOCATION_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+    private static final int LOCATION_REQUEST = 1337;
+
 
     TextView tvEnabledGPS;
     TextView tvStatusGPS;
@@ -39,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
             checkEnabled();
         }
 
+        @SuppressLint("MissingPermission")
         @Override
         public void onProviderEnabled(String provider) {
             checkEnabled();
@@ -68,16 +78,40 @@ public class MainActivity extends AppCompatActivity {
         tvLocationNet = findViewById(R.id.tvLocationNet);
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(LOCATION_PERMS,LOCATION_REQUEST);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean all_permission_granted = true;
+        for (int result : grantResults){
+            if (result != PackageManager.PERMISSION_GRANTED ) {
+                all_permission_granted = false; break;
+            }
+        }
+
+        if (all_permission_granted){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000 * 10, 10, locationListener);
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 1000 * 10, 10,
+                    locationListener);
+            checkEnabled();
+        }
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(
-                        this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -87,12 +121,15 @@ public class MainActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+
+        else{
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                 1000 * 10, 10, locationListener);
         locationManager.requestLocationUpdates(
                 LocationManager.NETWORK_PROVIDER, 1000 * 10, 10,
                 locationListener);
         checkEnabled();
+        }
     }
 
     @Override
